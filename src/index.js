dbe7032317e1b5fa71adff99d1cbdce1939dcca9
elminsterrr <przemyslaw.glab@gmail.com>
2018-01-20 15:06:33 +0100

    Add dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import './index.css';
 import App from './components/App/App.js';
 import registerServiceWorker from './registerServiceWorker';
 
+const createStoreWithMiddleware = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware}>
+    <App />
+  </Provider>,
+  document.getElementById('application')
+);
