1dfd1d0818bf8dc5b0bf95eef692be1256370b6c
elminsterrr <przemyslaw.glab@gmail.com>
2018-01-28 02:44:41 +0100

    Done ButtonBackToTop css

--- src/components/ButtonBackToTop/index.js
+++ src/components/ButtonBackToTop/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './style.css';
 
 const ButtonBackToTop = () => (
-  <div className="aligner">
-    <div className="scroll-top">
+  <div className="ButtonBackToTop-aligner">
+    <div className="ButtonBackToTop-scroll-top">
       <button
         className="btn btn-secondary"
         onClick={() => window.scrollTo(0, 0)}
