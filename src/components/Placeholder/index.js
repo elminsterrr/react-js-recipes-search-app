39b526802ce336335f8efa77ca3a3f953cf60a08
elminsterrr <przemyslaw.glab@gmail.com>
2018-02-08 03:09:15 +0100

    Change welcome info

--- src/components/Placeholder/index.js
+++ src/components/Placeholder/index.js
@@ -4,8 +4,8 @@ import './style.css';
 const Placeholder = () => (
   <div className="Placeholder-div">
     <p className="Placeholder-text">
-      Please enter cooking ingredients (example: chicken, honey, salt ) to
-      begin.
+      Please enter multiple ingredients (separated by commas) to begin
+      searching.
     </p>
     <p className="Placeholder-text">
       You can also click each ingredient from ingredients list to start a new
